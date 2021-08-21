003622e47816648dc441d681dd06ccb73cedf15f
srisha7 <76961848+srisha7@users.noreply.github.com>
2021-08-21 19:56:00 +0530

    Add files via upload

---
+++ project.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>HOSTEL ALLOTMENT</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<title>Hostels</title>
+<style>
+.main{
+position:relative;
+}
+
+.name{
+position:absolute;
+top:10%;
+left:30%;
+}
+li a{
+color:black;
+text-decoration:none;
+}
+
+
+.container{
+    width: 500px;
+    height: 300px;
+    text-align: center;
+    color: white;
+    margin: 0 auto;
+    background-color: grey;
+    border-radius: 04px;
+    margin: 0 auto;
+    margin-top: -400px;
+}
+
+
+</style>
+</head>
+<body>
+<div class="main">
+<img src="image 1.jpg" style="height:130px;width:1200px;"><br>
+<img src="image 2.jpg" style="height:110px;width:300px;"><br>
+<img src="image 3.jpg" style="height:110px;width:300px;"><br>
+<img src="image 4.jpg" style="height:120px;width:300px;"><br>
+<img src="image 5.jpg" style="height:110px;width:300px;">
+</div>
+<div class="container">
+<h1 style="color:Black;"><center>Login</center></h1>
+<br>
+<br>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/cwk/partial"><i>Login As</i></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+        <a class="nav-link" href="s11.php"><h5>Student</h5></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php"><h5>Admin</h5></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="w1.php"><h5>Warden</h5></a>
+      </li>
+      </ul>
+  </div>
+</nav>
+
+
+   
+
+</div>
+</body>
+</html>
